c185f101ae079aa02c472bc23ad750d80449a7a5
Wagner Umezaki <wagner.umezaki.dev@gmail.com>
2019-05-17 18:40:58 -0300

    Initial commit

---
+++ test/simple_clojure_api/core_test.clj
@@ -0,0 +1,7 @@
+(ns simple-clojure-api.core-test
+  (:require [clojure.test :refer :all]
+            [simple-clojure-api.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
